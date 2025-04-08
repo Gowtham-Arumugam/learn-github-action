831a6940677b4e834dcf095fc2976980316fab58
gowthamArumugam <gowthamarumugam@outlook.com>
2025-04-08 11:50:08 +0530

    added env step

---
+++ Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    //list all env variables
+    public static void main(String[] args) {
+       System.getenv().forEach((key, value) -> System.out.println(key + ": " + value));
+        
+        //get and print specific env variables
+        String javaHome = System.getenv("javatest");
+
+        System.out.println("\nSpecific Environment Variables:");
+        System.out.println("JAVA_HOME: " + javaHome);
+    }
+}
